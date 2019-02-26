80123ad0fbeaaf5ab79b1a271bea70ffa03efcb7
Jose Salgado <josetomassalgado91@gmail.com>
2019-02-26 10:06:31 -0400

    solucionar error ruta

--- public/js/custom.js
+++ public/js/custom.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
                 if (e) { 
                 	
                 	$.ajax({
-			            url: url+id,
+			            url: url+"/"+id,
 			            type: 'POST', 
 			            data: {
 			                "id": id,
