68cd447a9cad64492ddc2712ff6715753979368d
Eimar Urbina <eimar@e-abclearning.com>
2019-05-06 00:18:52 -0400

    Tours como categorias y modalidades de tour como productos

--- database/migrations/2019_02_26_021308_crear_table_tours.php
+++ database/migrations/2019_02_26_021308_crear_table_tours.php
@@ -16,22 +16,10 @@ class CrearTableTours extends Migration
         Schema::create('tours', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->nullable();
-            $table->integer('duracion')->nullable();
             $table->integer('precio')->nullable();
             $table->string('short_description', 100)->nullable();
             $table->text('long_description')->nullable();
-            $table->text('include')->nullable();//Lo que incluye el tour
-            $table->text('restrictions')->nullable();//Restricciones
-            $table->text('no_forget')->nullable();//No olvidar
-            $table->text('consider')->nullable();// Cosas a considerar
-            $table->boolean('adults')->nullable();
-            $table->boolean('children')->nullable();
-            $table->boolean('infants')->nullable();
-            $table->boolean('buggies')->nullable();
             $table->boolean('status')->nullable();
-            $table->boolean('important')->nullable();
-            $table->string('days')->nullable();//dias disponibles: 1=lunes, 0=domingo
-            $table->integer('likes')->nullable();
             $table->string('foto', 20)->nullable();//foto principal
             $table->string('fotos')->nullable();//galeria de fotos
                     
---
+++ database/migrations/2019_02_26_021309_crear_table_modalidad.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CrearTableModalidad extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('modalidades', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->integer('duracion')->nullable();
+            $table->integer('precio')->nullable();
+            $table->string('short_description', 100)->nullable();
+            $table->text('long_description')->nullable();
+            $table->text('include')->nullable();//Lo que incluye el tour
+            $table->text('restrictions')->nullable();//Restricciones
+            $table->text('no_forget')->nullable();//No olvidar
+            $table->text('consider')->nullable();// Cosas a considerar
+            $table->boolean('adults')->nullable();
+            $table->boolean('children')->nullable();
+            $table->boolean('infants')->nullable();
+            $table->boolean('status')->nullable();//visible o no
+            $table->boolean('important')->nullable();//Si es importante
+            $table->string('days')->nullable();//dias disponibles: 1=lunes, 0=domingo
+            $table->integer('likes')->nullable();
+            $table->string('foto', 20)->nullable();//foto principal
+            $table->string('fotos')->nullable();//galeria de fotos
+            $table->string('keywords')->nullable();//para filtros de busqueda
+                    
+            $table->integer('tour_id')->unsigned()->nullable();
+            $table->foreign('tour_id')->references('id')->on('tours')->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('modalidades');
+    }
+}
